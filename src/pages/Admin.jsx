8b89753010b955c2ea5e0434a2541e9c2ec1c177
zainop01 <zainop001@gmail.com>
2024-09-01 02:52:16 -0700

    Set Admin Routes and Make Side Nav , SideNav dropdown Pending

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TopNav from '../components/TopNav'
+import SideNav from '../components/SideNav'
+
+const Admin = () => {
+  return (
+    <div>
+      {/* <h1>Zain OP</h1> */}
+      <TopNav/>
+      <SideNav/>
+    </div>
+  )
+}
+
+export default Admin
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Routes , Route} from 'react-router-dom'
 import Widgets from '../widgets'
 import Home from '../pages/Home'
+import Admin from '../pages/Admin'
 
 const Routers = () => {
   return (
    <Routes>
         <Route path='/widgets' element={<Widgets/>}/>
         <Route path='/' element={<Home/>}/>
+        <Route path='/admin' element={<Admin/>}/>
+
    </Routes>
   )
 }
